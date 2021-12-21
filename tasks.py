a11e030a83c703f8ee4cd8a6350c343e93a5f0d7
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-12-21 14:30:17 -0300

    Alteração no nome

--- tasks.py
+++ tasks.py
@@ -6,5 +6,5 @@ app = Celery('user')
 app.config_from_object('celeryconfig')
 
 @app.task(name='create-user')
-def user(user):
+def create_user(user):
   return requests.post('http://localhost:5001/api/users', json=user)
